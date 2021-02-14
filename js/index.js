4d75aecc36931462dcbb1d06d6207fc1c988689c
timschutz <70916625+timschutz@users.noreply.github.com>
2021-02-14 17:24:15 -0600

    init layout, bootstrap, content

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".nav-link").click(function(event){     
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000);
+    });
+});
