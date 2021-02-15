f9106e8e1fba1241c542ba6bad1f39a2cf783f26
timschutz <70916625+timschutz@users.noreply.github.com>
2021-02-15 16:09:56 -0600

    tweaked for mobile, added sticky nav

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(".nav-link").click(function(event){     
         event.preventDefault();
-        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000);
+        $('html,body').animate({scrollTop:$(this.hash).offset().top -150}, 1000);
     });
 });
